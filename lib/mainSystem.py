81289f593790b983f2e4d5e84c11757dd282c724
kmikus <kevinpmikus@gmail.com>
2017-11-22 02:15:40 -0500

    main file up and running.

---
+++ lib/mainSystem.py
@@ -0,0 +1,22 @@
+import rabbit, json, mongolog
+
+url = "https://jsonplaceholder.typicode.com/posts/1/comments"
+
+# The void is there to give a name to positional argument in order to make the dispatcher work but it is not used
+steps = [
+	{"name": "curl", "action": lambda void: rabbit.Curler(url).getJson(), "displayMessage": "Getting JSON data from " + url},
+	{"name": "encrypt", "action": lambda payload: rabbit.Encryptor(payload).encrypt(), "displayMessage": "Encrypting data..."},
+	{"name": "rabbitSend", "action": lambda payload: rabbit.Sender(payload).send(), "displayMessage": "Sending RabbitMQ message..."},
+	{"name": "rabbitReceive", "action": lambda void: rabbit.Receiver().receive(), "displayMessage": "Receiving RabbitMQ message..."},
+	{"name": "decrypt", "action": lambda payload: rabbit.Decryptor(payload).decrypt(), "displayMessage": "Decrypting data..."}
+]
+
+payload = None
+log = mongolog.Logger()
+
+for step in steps:
+	payload = step["action"](payload)
+	record = log.prepRecord(step["name"], payload)
+	log.insertRecord(record)
+	print(step["displayMessage"])
+	print("Done.")
