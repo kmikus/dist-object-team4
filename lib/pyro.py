512e07cabfbf3f71da8f658cd25c2fdb7b8f8042
root <root@Eugene.localdomain>
2017-11-26 14:25:58 -0500

    added header

--- lib/pyro.py
+++ lib/pyro.py
@@ -1,3 +1,10 @@
+# Project: Team 4 Distributed System Project
+# Purpose Details: Demonstrate full cycle distributed system using json payloads and various python libraries/frameworks
+# Course: IST411
+# Author: Eugene Matavitski
+# Date Developed: 2017-11-10
+# Last Date Changed: 2017-11-26
+# Rev: 0.2
 import Pyro4, zlib, sys, base64, json
 SERVERNAME = "localhost"
 
@@ -34,5 +41,5 @@ class Sender(object):
                 except Exception as e:
                         print(e)
         def get_checksum(self):
-                        print("Calculating and returning checksum")
-                        return zlib.crc32(self._data.encode('utf8'))
+                print("Calculating and returning checksum")
+                return zlib.crc32(self._data.encode('utf8'))
