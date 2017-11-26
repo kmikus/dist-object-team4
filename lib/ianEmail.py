cea88e4779151ac938cee8e06bbf1347c84532aa
Ian Roach <iroach7438@gmail.com>
2017-11-26 11:16:08 -0500

    editing ianEmail.py

--- lib/ianEmail.py
+++ lib/ianEmail.py
@@ -15,6 +15,6 @@ class Email:
             msg['Subject'] = self.subject
             msg['From'] = self.fromAdd
             msg['To'] = self.toAdd
-            self.connection.sendmail(fromAdd, toAdd, msg.as_string())
+            self.connection.sendmail(self.fromAdd, self.toAdd, self.msg.as_string())
         except Exception as e:
             print("Error {}".format(e.args[0]))
