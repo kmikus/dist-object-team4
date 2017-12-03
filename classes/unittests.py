e0d0a82f5eb09a76fe5ce91d3356fb3361b0ed66
kmikus <kevinpmikus@gmail.com>
2017-12-03 02:53:14 -0500

    added more unit tests

--- classes/unittests.py
+++ classes/unittests.py
@@ -8,10 +8,32 @@
 
 # Unit tests
 
-import unittest, kevinRabbit, eugenePyro, zlib
+import unittest, teamFourMongolog, kevinRabbit, eugenePyro, zlib
 
+# DO NOT EDIT THESE VALUES
 testJson = '{"test": "test"}'
+testJsonEncrypted = b'\x9b\xc5\x1dQm\x82\x99b\xf8\xc2\xe5\xac\x19\xcb\xf7\xb5f\x08\x0c\x8b\x87\xa5\xd8\xa1\xfb\x01\x1a\xa4\xf3kG\x88'
+testEventString = "Unit Testing"
+testPayload = b"Testing"
 url = "https://jsonplaceholder.typicode.com/posts/1/comments"
+
+class MongologTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.log = teamFourMongolog.Logger()
+		self.testRecord = self.log.prepRecord(testEventString, testPayload)
+
+	def tearDown(self):
+		self.log = None
+
+	# Methods testing
+
+	def test_prepRecord(self):
+		self.assertEqual(len(self.testRecord), 3)
+		self.assertIs(type(self.testRecord), dict)
+
+	def test_insertRecord(self):
+		self.assertIsNot(self.log.insertRecord(testEventString, testPayload), None)
 
 class KevinRabbitTest(unittest.TestCase):
 
@@ -21,7 +43,7 @@ class KevinRabbitTest(unittest.TestCase):
 		self.receiver = kevinRabbit.Receiver()
 		self.curler = kevinRabbit.Curler(url)
 		self.encryptor = kevinRabbit.Encryptor(testJson)
-		self.decryptor = kevinRabbit.Decryptor(testJson)
+		self.decryptor = kevinRabbit.Decryptor(testJsonEncrypted)
 
 	def tearDown(self):
 		self.sender = None
@@ -32,12 +54,28 @@ class KevinRabbitTest(unittest.TestCase):
 
 	# Methods testing
 
+	def test_send(self):
+		self.assertTrue(self.sender.send())
+
+	def test_receive(self):
+		self.sender.send()
+		self.assertIsNot(self.receiver.receive(), None)
+
 	def test_getJson(self):
 		self.assertIsNot(self.curler.getJson(), None)
 
 	def test_pad(self):
 		self.assertEqual(len(self.encryptor.pad()), 32)
 		self.assertIsNot(self.encryptor.pad(), None)
+
+	def test_encrypt(self):
+		self.assertEqual(len(self.encryptor.encrypt()), 32)
+		self.assertIsNot(self.encryptor.encrypt(), None)
+
+	def test_decrypt(self):
+		ciphertext = self.encryptor.encrypt()
+		self.assertEqual(self.decryptor.decrypt(), testJson)
+		self.assertIs(type(self.decryptor.decrypt()), str)
 
 class EugenePyroTest(unittest.TestCase):
         
@@ -68,5 +106,5 @@ class EugenePyroTest(unittest.TestCase):
 
 # keep at bottom
 if __name__ == "__main__":
-        unittest.main()
+        unittest.main(buffer=True)
 
