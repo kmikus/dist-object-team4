a36cb9a7a20610ad7adbb54c5863a51d4887a19d
root <root@Eugene.localdomain>
2017-12-06 00:02:10 -0500

    added try catch blocks to botNode.py

--- classes/botNode.py
+++ classes/botNode.py
@@ -7,38 +7,52 @@
 
 import ianSftp, eugenePyro, ianEmail, ianHmac, teamFourMongolog
 from threading import Thread
-
+""" class that is responsible for actions of the bottom node"""
 def threaded_function(PyroObject):
+   """method to run pyro as separate thred"""
    PyroObject.startPyro()
 
 if __name__ == "__main__":
-
-   log = teamFourMongolog.Logger()
-   log.insertRecord("Bottom node start", None)
-
+   try:
+      log = teamFourMongolog.Logger()
+      log.insertRecord("Bottom node start", None)
+   except Exception as e:
+      print(e)
    pyro_object = eugenePyro.Sender()
    thread = Thread(target = threaded_function, args = (pyro_object,))
    thread.start()
 
    # pull file from sftp server
    fname = "payloadTeam4.json"
-   payload = ianSftp.Client().get(fname)
-   payload = ianHmac.Hmac().unwrap().encode("utf-8")
+   try:
+      payload = ianSftp.Client().get(fname)
+      payload = ianHmac.Hmac().unwrap().encode("utf-8")
+   except Exception as e:
+      print(e)
+      print("Something is wrong with a file")
    print(payload)
    log.insertRecord("Bottom node receive", payload)
 
    # sending email
-   emailSender = ianEmail.Email("Test subject", "kzm5599@psu.edu", "kzm5599@psu.edu")
-   emailSender.sendMail(payload.decode("utf-8"))
-
+   try:
+      emailSender = ianEmail.Email("Test subject", "kzm5599@psu.edu", "kzm5599@psu.edu")
+      emailSender.sendMail(payload.decode("utf-8"))
+   catch Excteption as e:
+      print(e)
+      print("Couldn't send email")
    # updating payload data with payload recieved in Sftp
-   pyro_object.set_data(payload)
-
+   try:
+      pyro_object.set_data(payload)
+   except Exception as e:
+      print(e)
    # promting user to start left node
    print('Please run leftNode.py')
 
    # waiting for client to read payload
-   while(pyro_object.get_iteration()==0):
-      pass
-   pyro_object.kill_pyro()
-   log.insertRecord("Bottom node send", payload)
+   try:
+      while(pyro_object.get_iteration()==0):
+         pass
+      pyro_object.kill_pyro()
+      log.insertRecord("Bottom node send", payload)
+   except Exception as e:
+      print(e)
